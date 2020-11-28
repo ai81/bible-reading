68e6088f784b4549edcd5ad3218a1c22b481dad9
Anton Ivanov <antonio@yandex-team.ru>
2020-11-28 17:12:58 +0300

    add 2021 holidays support 

--- Bible Reading/HolidaysHolder.swift
+++ Bible Reading/HolidaysHolder.swift
@@ -63,6 +63,53 @@ class HolidaysHolder {
         return cal.ordinality(of: .day, in: .year, for: date)!
     }
     
+    private static let dateToHoliday2021: [Int: Holiday] = Dictionary ([
+        
+        "1.1": Holiday(type: .Celebration, name: "Пресвятая Богородица Мария"),
+        "1.6": Holiday(type: .Celebration, name: "Богоявление"),
+        "1.10": Holiday(type: .Holiday, name: "Крещение Господне"),
+        "1.25": Holiday(type: .Holiday, name: "Обращение св. Апостола Павла"),
+        "2.2": Holiday(type: .Holiday, name: "Сретение Господне"),
+        "2.17": Holiday(type: .Holiday, name: "Пепельная Среда"),
+        "2.22": Holiday(type: .Holiday, name: "Кафедра св. Апостола Петра"),
+        "3.19": Holiday(type: .Celebration, name: "Св. Иосиф, Обручник Пресвятой Девы Марии"),
+        "3.25": Holiday(type: .Celebration, name: "Благовещение Господне"),
+        "3.28": Holiday(type: .Celebration, name: "Вербное Воскресенье"),
+        "4.4": Holiday(type: .Celebration, name: "Пасха. Светлое Христово Воскресение"),
+        "5.6": Holiday(type: .Holiday, name: "Св. Георгий Победоносец"),
+        "5.13": Holiday(type: .Celebration, name: "Вознесение Господне"),
+        "5.14": Holiday(type: .Holiday, name: "Св. Матфей Апостол"),
+        "5.23": Holiday(type: .Celebration, name: "Сошествие Святого Духа. Пятидесятница"),
+        "5.30": Holiday(type: .Celebration, name: "Персвятая Троица"),
+        "5.31": Holiday(type: .Holiday, name: "Посещение Пресвятой Девой Марией Елизаветы"),
+        "6.6": Holiday(type: .Celebration, name: "Пресвятые Тело и Кровь Господни"),
+        "6.11": Holiday(type: .Celebration, name: "Святейшее Сердце Иисуса"),
+        "6.24": Holiday(type: .Celebration, name: "Рождество св. Иоанна Крестителя"),
+        "6.29": Holiday(type: .Celebration, name: "свв. Пётр и Павел, Апостолы"),
+        "7.3": Holiday(type: .Holiday, name: "Св. Фома Апостол"),
+        "7.22": Holiday(type: .Holiday, name: "Св. Мария Магдалина"),
+        "8.6": Holiday(type: .Holiday, name: "Преображение Господне"),
+        "8.10": Holiday(type: .Holiday, name: "Св. Лаврентий, диакон и муч."),
+        "8.15": Holiday(type: .Celebration, name: "Успение Пресвятой Богородицы"),
+        "8.24": Holiday(type: .Holiday, name: "Св. Варфоломей, Апостол"),
+        "9.8": Holiday(type: .Holiday, name: "Рождество Пресвятой Богородицы"),
+        "9.14": Holiday(type: .Holiday, name: "Воздвижение Святого Креста"),
+        "9.21": Holiday(type: .Holiday, name: "Св. Матфей, Апостол и Евангелист"),
+        "9.29": Holiday(type: .Holiday, name: "Свв. Архангелы Михаил, Гавриил и Рафаил"),
+        "10.18": Holiday(type: .Holiday, name: "Св. Лука, Евангелист"),
+        "10.28": Holiday(type: .Holiday, name: "Свв. Симон и Иуда Фаддей, Апостолы"),
+        "11.1": Holiday(type: .Celebration, name: "Все Святые"),
+        "11.9": Holiday(type: .Holiday, name: "Освящение Латеранской Базилики"),
+        "11.21": Holiday(type: .Holiday, name: "Господь наш Иисус Христос, Царь Вселенной"),
+        "11.30": Holiday(type: .Celebration, name: "св. Андрей, Апостол"),
+        "12.8": Holiday(type: .Celebration, name: "Непорочное Зачатие Пресвятой Девы Марии"),
+        "12.25": Holiday(type: .Celebration, name: "Рождество Христово"),
+        "12.26": Holiday(type: .Holiday, name: "Святое Семейство - Иисус, Мария и Иосиф"),
+        "12.27": Holiday(type: .Holiday, name: "Св. Иоанн, Апостол и Евангелист"),
+        "12.28": Holiday(type: .Holiday, name: "Свв. Невинные младенцы Вифлеемские"),
+        
+        ].map { (HolidaysHolder.strToOrdinal(str: $0, year: 2021), $1)    })
+
     private static let dateToHoliday2020: [Int: Holiday] = Dictionary ([
         
         "1.1": Holiday(type: .Celebration, name: "Пресвятая Богородица Мария"),
@@ -228,6 +275,7 @@ class HolidaysHolder {
         2017: dateToHoliday2017,
         2018: dateToHoliday2018,
         2019: dateToHoliday2019,
-        2020: dateToHoliday2020
+        2020: dateToHoliday2020,
+        2021: dateToHoliday2021
     ]
 }
