5bf9b40592a01386efe6c0c6e417c987069d673e
Anton Ivanov <antonio@yandex-team.ru>
2021-01-02 11:20:15 +0300

    migrate to new notifications framework

--- Bible Reading/NotificationsSupport.swift
+++ Bible Reading/NotificationsSupport.swift
@@ -8,10 +8,11 @@
 
 import Foundation
 import UIKit
-
+import UserNotifications
+    
 class NotificationsSupport {
     public static func reinstallNotifications() {
-        UIApplication.shared.cancelAllLocalNotifications()
+        UNUserNotificationCenter.current().removeAllPendingNotificationRequests()
         
         let nAboutReading = UserDefaults.standard.object(forKey: "day_reading_notification").map{$0 as! Bool}
         let nHolyday0 = UserDefaults.standard.object(forKey: "holiday_0_day").map{$0 as! Bool}
@@ -19,18 +20,20 @@ class NotificationsSupport {
         let nHolyday3 = UserDefaults.standard.object(forKey: "holiday_3_day").map{$0 as! Bool}
         
         let today = Date()
-        var components = Calendar.current.dateComponents([.year, .month, .day], from: today)
+        var components = Calendar.current.dateComponents([.year, .month, .day, .hour, .minute, .second], from: today)
+        
         components.hour = 10
         components.minute = 0
         components.second = 0
         components.day = components.day! + 1
+        // components.second = components.second! + 10 // for testing
         
         let daysForward = 14
-        var date = Calendar.current.date(from: components)!
-        var datePlusDay = Calendar.current.date(byAdding: .day, value: 1, to: date)!
-        var datePlus3Day = Calendar.current.date(byAdding: .day, value: 3, to: date)!
         
         for _ in 0...daysForward {
+            let date = Calendar.current.date(from: components)!
+            let datePlusDay = Calendar.current.date(byAdding: .day, value: 1, to: date)!
+            let datePlus3Day = Calendar.current.date(byAdding: .day, value: 3, to: date)!
             var text = String()
             if nHolyday0 ?? true {
                 if let holiday = HolidaysHolder.findHoliday(date: date) {
@@ -64,16 +67,17 @@ class NotificationsSupport {
                 }
             }
             if !text.isEmpty {
-                let notification = UILocalNotification()
-                notification.alertBody = text
-                notification.fireDate = date
-                notification.timeZone = TimeZone.current
-                notification.soundName = UILocalNotificationDefaultSoundName
-                UIApplication.shared.scheduleLocalNotification(notification)
+                let trigger = UNCalendarNotificationTrigger(dateMatching: components, repeats: false)
+
+                let center = UNUserNotificationCenter.current()
+                let content = UNMutableNotificationContent()
+                content.body = text
+                content.sound = UNNotificationSound.default
+                
+                let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
+                center.add(request)
             }
-            date = Calendar.current.date(byAdding: .day, value: 1, to: date)!
-            datePlusDay = Calendar.current.date(byAdding: .day, value: 1, to: datePlusDay)!
-            datePlus3Day = Calendar.current.date(byAdding: .day, value: 1, to: datePlus3Day)!
+            components.day = components.day! + 1
         }
     }
 }
